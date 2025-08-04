3a085683f173f39258a85a04bfa73de424a64336
Nadun00 <it22608840@my.sliit.lk>
2025-08-04 18:22:47 +0530

    create post

---
+++ server/models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    category: {
+        type: String,
+        enum: ['teaching', 'learning'],
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Post', 'PostScema');
