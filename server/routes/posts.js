558ef24fcd9f57f1ab01a4a703645d31f9eb221a
Nadun00 <it22608840@my.sliit.lk>
2025-08-06 20:15:15 +0530

    create new post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
+
+//create a new post
+router.post('/', async(req, res) =>{
+    try{
+        const{ title, description, category} = req.body;
+
+        const newPost = new Post({
+            title,
+            description;
+            category
+        });
+    }
+})
