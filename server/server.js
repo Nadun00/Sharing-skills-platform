44257bf7495993cb9f1698a72dfd1812bb219cc5
Nadun00 <it22608840@my.sliit.lk>
2025-08-25 15:48:54 +0530

    connect route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
+const postRoutes = require ('./routes/posts');
+app.use('/api/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server running on port ${PORT}'));
